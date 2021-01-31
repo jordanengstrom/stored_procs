d8c914b3632f99eaf1ac581d96eefe787bf0e929
jordanengstrom <jordan.engstrom@gmail.com>
2021-01-31 12:56:00 -0700

    covered basics of creating, running, and dropping stored procedures

---
+++ CreateProcedure.sql
@@ -0,0 +1,11 @@
+USE Contacts;
+
+GO
+DROP PROCEDURE IF EXISTS dbo.SelectContacts;
+
+GO
+CREATE PROCEDURE dbo.SelectContacts
+AS
+BEGIN;
+	SELECT * FROM dbo.Contacts WHERE FirstName = 'Grace';
+END;
